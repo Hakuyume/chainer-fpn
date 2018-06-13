5fc22ca43713410b18574c4abc0106f6e1478153
Toru Ogawa <Hakuyume@users.noreply.github.com>
2018-06-14 01:49:11 +0900

    add gpu option

--- demo.py
+++ demo.py
@@ -12,12 +12,17 @@ from faster_rcnn_fpn import FasterRCNNFPNResNet101
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--gpu', type=int, default=-1)
     parser.add_argument('model')
     parser.add_argument('image')
     args = parser.parse_args()
 
     model = FasterRCNNFPNResNet101(n_fg_class=len(coco_bbox_label_names))
     chainer.serializers.load_npz(args.model, model)
+
+    if args.gpu >= 0:
+        chainer.cuda.get_device_from_id(args.gpu).use()
+        model.to_gpu()
 
     img = utils.read_image(args.image)
     bboxes, labels, scores = model.predict([img])
