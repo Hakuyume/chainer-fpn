0a14ac786538422b37f1a78260732e775ef9ea5e
Toru Ogawa <Hakuyume@users.noreply.github.com>
2018-06-14 02:12:23 +0900

    add eval script

---
+++ eval_coco.py
@@ -0,0 +1,54 @@
+import argparse
+
+import chainer
+from chainer import iterators
+
+from chainercv.datasets import coco_bbox_label_names
+from chainercv.datasets import COCOBboxDataset
+from chainercv.evaluations import eval_detection_coco
+from chainercv.utils import apply_to_iterator
+from chainercv.utils import ProgressHook
+
+from faster_rcnn_fpn import FasterRCNNFPNResNet101
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model')
+    parser.add_argument('--gpu', type=int, default=-1)
+    args = parser.parse_args()
+
+    model = FasterRCNNFPNResNet101(n_fg_class=len(coco_bbox_label_names))
+    chainer.serializers.load_npz(args.model, model)
+
+    if args.gpu >= 0:
+        chainer.cuda.get_device_from_id(args.gpu).use()
+        model.to_gpu()
+
+    model.use_preset('evaluate')
+
+    dataset = COCOBboxDataset(
+        split='minval',
+        use_crowded=True,
+        return_area=True,
+        return_crowded=True)
+    iterator = iterators.SerialIterator(
+        dataset, 1, repeat=False, shuffle=False)
+
+    in_values, out_values, rest_values = apply_to_iterator(
+        model.predict, iterator, hook=ProgressHook(len(dataset)))
+    # delete unused iterators explicitly
+    del in_values
+
+    pred_bboxes, pred_labels, pred_scores = out_values
+    gt_bboxes, gt_labels, gt_area, gt_crowded = rest_values
+
+    result = eval_detection_coco(
+        pred_bboxes, pred_labels, pred_scores,
+        gt_bboxes, gt_labels, gt_area, gt_crowded)
+
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
