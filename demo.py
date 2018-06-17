a945244421910217f10f4b78c0ccff37460d9b84
Toru Ogawa <Hakuyume@users.noreply.github.com>
2018-06-17 15:38:55 +0900

    rename args

--- demo.py
+++ demo.py
@@ -15,7 +15,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu', type=int, default=-1)
     parser.add_argument('--model', choices=('resnet50', 'resnet101'))
-    parser.add_argument('pretrained-model')
+    parser.add_argument('pretrained_model')
     parser.add_argument('image')
     args = parser.parse_args()
 
--- eval_coco.py
+++ eval_coco.py
@@ -17,7 +17,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu', type=int, default=-1)
     parser.add_argument('--model', choices=('resnet50', 'resnet101'))
-    parser.add_argument('pretrained-model')
+    parser.add_argument('pretrained_model')
     args = parser.parse_args()
 
     if args.model == 'resnet50':
