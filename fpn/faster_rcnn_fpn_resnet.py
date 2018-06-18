2386ef18016b9f468f339e8cbeb1db69d36cdca0
Toru Ogawa <Hakuyume@users.noreply.github.com>
2018-06-18 12:12:51 +0900

    remove AffineChannel

--- fpn/faster_rcnn_fpn_resnet.py
+++ fpn/faster_rcnn_fpn_resnet.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-import chainer
 import chainer.functions as F
 
 import chainercv
@@ -19,12 +18,6 @@ def _make_fpn(cls):
     base.mean = np.array((122.7717, 115.9465, 102.9801))[:, None, None]
     base.pool1 = lambda x: F.max_pooling_2d(
         x, 3, stride=2, pad=1, cover_all=False)
-    for link in base.links():
-        if hasattr(link, 'bn'):
-            size = len(link.bn.avg_mean)
-            del link.bn
-            with link.init_scope():
-                link.bn = AffineChannel(size)
 
     return FPN(base, len(base.pick), (1 / 4, 1 / 8, 1 / 16, 1 / 32, 1 / 64))
 
@@ -49,16 +42,3 @@ class FasterRCNNFPNResNet101(FasterRCNN):
             rpn=RPN(extractor.scales),
             head=Head(n_fg_class + 1, extractor.scales),
         )
-
-
-class AffineChannel(chainer.Link):
-
-    def __init__(self, size):
-        super().__init__()
-        self.gamma = np.empty(size, dtype=np.float32)
-        self.beta = np.empty(size, dtype=np.float32)
-        self.register_persistent('gamma')
-        self.register_persistent('beta')
-
-    def __call__(self, x):
-        return x * self.gamma[:, None, None] + self.beta[:, None, None]
